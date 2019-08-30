2609495c088237e4d940789b69ae0a243e1df1e4
Christoph Keßler <ck@plskthx.org>
2019-08-30 18:41:49 +0200

    Add License information.

--- src/ryu.lisp
+++ src/ryu.lisp
@@ -1,6 +1,28 @@
-(defpackage :ryu-cl (:use :common-lisp)
-            (:nicknames :ryu)
-            (:export #:float-to-string))
+;;; Ported from C to Common Lisp.
+;;; Source: https://github.com/ulfjack/ryu
+;;;
+;;; Copyright 2018 Ulf Adams
+;;;
+;;; The contents of this file may be used under the terms of the Apache License,
+;;; Version 2.0.
+;;;
+;;;    (See accompanying file LICENSE-Apache or copy at
+;;;     http://www.apache.org/licenses/LICENSE-2.0)
+;;;
+;;; Alternatively, the contents of this file may be used under the terms of
+;;; the Boost Software License, Version 1.0.
+;;;    (See accompanying file LICENSE-Boost or copy at
+;;;     https://www.boost.org/LICENSE_1_0.txt)
+;;;
+;;; Unless required by applicable law or agreed to in writing, this software
+;;; is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+;;; KIND, either express or implied.
+
+(defpackage :ryu-cl
+  (:documentation "An implementation of the Ryu float to string converter by Ulf Adams: https://github.com/ulfjack/ryu .")
+  (:use :common-lisp)
+  (:nicknames :ryu)
+  (:export #:float-to-string))
 
 (in-package :ryu-cl)
 
